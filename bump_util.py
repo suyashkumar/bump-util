10ead847448c6c73c7a675700b3c1ab2442d4ffa
Suyash Kumar <suyash.kumar@duke.edu>
2015-08-20 01:09:13 -0400

    Starting to add single player functionality

--- bump_util.py
+++ bump_util.py
@@ -5,7 +5,7 @@ Common Bumper Pool Functions/Utilities
 """
 
 from parsing import * # Import parse functions
-
+from elo import elo_util
 
 #Loads stored playerData in json format, returns dict
 #of player data
@@ -32,7 +32,7 @@ def playerCompare(playerOne, playerTwo, playerData):
         playerTwo:  String name (key) for second player 
         playerData: the dict holding all player data  
     Returns: 
-        Dict with win values and dates for all games played by playerOne against playerTwo (in that perspective only)
+        returnData: Dict with win values and dates for all games played by playerOne against playerTwo (in that perspective only)
     """
        
     p1Data=playerData[playerOne]
@@ -41,8 +41,23 @@ def playerCompare(playerOne, playerTwo, playerData):
     	if (p1Data['opponent'][i]==playerTwo):
     		returnData['win'].append(p1Data['games'][i])
     		returnData['date'].append(p1Data['dates'][i])
-    return returnData
+    return returnData #TODO: include elohistory in this dict, tab this over to save iterations
 
+def singleInfo(player, playerData, csvPath):
+    """
+    Returns playerData information for the given player along with full elo history
+    Args:
+        player:     the player "key" string
+        playerData: playerData dict holding parsed data for all players
+        csvPath:    path to the data csv (currently for elo history calculation 
+    Returns:
+        returnData: dict holding playerData and eloHistory for player. 
+    """
+    pData=playerData[player] # Get information for this player
+    playerElos=elo_util.calculateElos(csvPath) # Recalc all Elos TODO: will be replaced by database read allowing for periodic recalcs 
+    pElo=elo_util.getFullEloHistory(playerElos,player) # Get ELO history for this player
+    returnData={'pData':pData, 'pElo':pElo} # Return dictionary holding player info
+    return returnData
 
 
 if __name__ == '__main__':
